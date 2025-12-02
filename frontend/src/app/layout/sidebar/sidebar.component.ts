bf433e3a7187bb4256c1a3ab0355e3660a7d5832
IsmaFerror <isma53428@gmail.com>
2025-12-02 10:56:17 +0100

    tailwing update

--- frontend/src/app/layout/sidebar/sidebar.component.ts
+++ frontend/src/app/layout/sidebar/sidebar.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css',
+  styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-
+  user = {
+    name: 'Admin Usuario',
+    email: 'admin@empresa.com',
+    initial: 'A'
+  };
+currentDate: any;
 }
