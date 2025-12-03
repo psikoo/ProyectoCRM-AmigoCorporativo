2041df6fa95a6e3fec66291952712524f8c95871
psikoo <hugohernandezmuelas03@gmail.com>
2025-12-03 13:51:28 +0100

    a

--- frontend/src/app/layout/sidebar/sidebar.component.ts
+++ frontend/src/app/layout/sidebar/sidebar.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink, RouterLinkActive } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-sidebar',
@@ -10,10 +11,29 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
-  user = {
-    name: 'Admin Usuario',
-    email: 'admin@empresa.com',
-    initial: 'A'
-  };
-currentDate: any;
+  name = 'Loading';
+  initial = 'A';
+  currentDate: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    let reqHeaders = {
+      "Authorization": this.getCookie("token"),
+      "Content-Type": "application/json"
+    }
+    this.http.get("http://localhost:8080/api/users", { headers: reqHeaders})
+      .subscribe((data:any) => {
+        this.name = "B"
+      });
+    }
+
+  getCookie(name: string) {
+    const cookies = document.cookie.split('; ');
+    for (let cookie of cookies) {
+      const [key, value] = cookie.split('=');
+      if (key === name) { return decodeURIComponent(value); }
+    }
+    return "null";
+  }
 }
